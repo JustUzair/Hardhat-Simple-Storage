21e81d3b331d603aae0db06a8bc5cbe006e4fd2c
Uzair Saiyed <uzairhajra76330@gmail.com>
2022-12-12 19:44:19 +0530

    Analysis of Tests

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,6 +2,8 @@ require("@nomiclabs/hardhat-waffle");
 require("dotenv").config();
 require("@nomiclabs/hardhat-etherscan");
 require("./tasks/block-number");
+require("hardhat-gas-reporter");
+require("solidity-coverage");
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 
@@ -15,6 +17,7 @@ require("./tasks/block-number");
 const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY;
 module.exports = {
     defaultNetwork: "hardhat",
     networks: {
@@ -27,5 +30,12 @@ module.exports = {
     solidity: "0.8.8",
     etherscan: {
         apiKey: ETHERSCAN_API_KEY,
+    },
+    gasReporter: {
+        enabled: true,
+        outputFile: "gas-report.txt",
+        noColors: true,
+        currency: "USD",
+        coinmarketcap: COINMARKETCAP_API_KEY,
     },
 };
